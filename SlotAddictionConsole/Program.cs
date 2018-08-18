32661dd6a42820b3c0bfcaff80459aa5f1f5df17
karimata1106 <karimata1106@gmail.com>
2018-08-18 17:59:01 +0900

    フィルタリング記念（泣）
    CoreLogicは日時ver.を参照するようにしているのでそっちをコンパイルおなしゃす

---
+++ SlotAddictionConsole/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlotAddictionCore.Models;
+using SlotAddictionLogic.Line;
+using SlotAddictionLogic.Models;
+
+namespace SlotAddictionConsoleForGaisen
+{
+    public class Program
+    {
+        #region フィールド
+        /// <summary>
+        /// ライン通知のトークン
+        /// </summary>
+        private static string _lineToken = "Stjd5Laa2ZPH6tZ6OIxpOJtxR9PbVMxt8kBC9Sa4OYU";
+
+        /// <summary>
+        /// テキストファイル名
+        /// </summary>
+        private const string TextFileName = "PreResult.txt";
+
+        #endregion
+
+        #region メソッド
+
+        private static async Task Main(string[] args)
+        {
+            var date = DateTime.Now;
+            //データが更新されない時間帯は取得を避けておく
+            if (date.Hour < 10
+                || date.Hour >= 22)
+            {
+#if DEBUG
+                date = date.AddDays(-1);
+#elif !DEBUG
+                return;
+#endif
+            }
+
+            //念のため
+            try
+            {
+                var (Output, OutputUri) = await GetAnalyseResult(date);
+                if (OutputUri.Any())
+                {
+                    //前回情報を取得する
+                    if (!File.Exists(TextFileName))
+                    {
+                        using (File.Create(TextFileName)) { }
+                    }
+
+                    //前回抽出結果に含まれていないURLがあれば出力する
+                    var sr = File.ReadAllLines(TextFileName);
+                    if (OutputUri.Except(sr).Any())
+                    {
+#if DEBUG
+                        _lineToken = "XLzPrFUKoS33P4QCAqO15M31c9RfDhYMJjgJoV5fIJX";
+#endif
+                        LineAlert.Message(_lineToken, Output);
+                        File.Delete(TextFileName);
+                        using (File.Create(TextFileName)) { }
+                        File.AppendAllLines(TextFileName, OutputUri);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                var hoge = 0;
+                //ignore
+            }
+
+#if !DEBUG
+            var folderPath = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            var folderName = Path.GetFileName(folderPath);
+            _lineToken = "XLzPrFUKoS33P4QCAqO15M31c9RfDhYMJjgJoV5fIJX";
+            LineAlert.Message(_lineToken, folderName + Environment.NewLine + "動作してるよ");
+#endif
+        }
+        /// <summary>
+        /// 遊技履歴から狙い目台の情報解析を行う
+        /// </summary>
+        /// <param name="searchDate"></param>
+        /// <returns></returns>
+        private static async Task<(string Output, List<string> OutputUri)> GetAnalyseResult(DateTime searchDate)
+        {
+            var output = new StringBuilder(string.Empty);
+            var outputUri = new List<string>();
+
+            var tenpoInfo = ExcelData.GetTenpoInfo();
+            var slotMachineSearchNames = ExcelData.GetSlotMachineSearchNames();
+
+            var analyseSlotData = new AnalyseSlotData(searchDate);
+            foreach (var slotMachineSearchName in slotMachineSearchNames)
+            {
+                //機種名を検索する文字列を取得して解析する
+                var aimMachineInfo = ExcelData.GetAimMachineInfo().Where(x => x.MachineName == slotMachineSearchName.Key).ToList();
+                await analyseSlotData.GetSlotDataAsync(tenpoInfo, slotMachineSearchName.Value, aimMachineInfo);
+
+                //狙える台がない場合
+                if (!analyseSlotData.SlotPlayDataCollection.Any())
+                {
+                    continue;
+                    output.AppendLine("(設置されて)ないです。");
+                }
+                else if (analyseSlotData.AimMachineCollection.Any())
+                {
+                    //機種名を出力
+                    output.AppendLine(Environment.NewLine + "【" + slotMachineSearchName.Key + "】");
+
+                    //ステータスでグルーピングする
+                    var aimMachineStatusGrouping = analyseSlotData.AimMachineCollection
+                        .OrderByDescending(x => x.CoinPrice) //コレクションの中身をコイン単価で降順に並べる
+                        .ThenByDescending(x => x.StartCount) //コレクションの中身をスタート数で降順に並べる
+                        .GroupBy(x => new
+                        {
+                            x.Status,
+                            x.ExceptValue,
+                        }) //ステータスと期待値でグループ化する
+                        .OrderByDescending(x => x.Key.ExceptValue); //グループを期待値で降順に並べる
+                    foreach (var aimMachines in aimMachineStatusGrouping)
+                    {
+                        output.AppendLine("[" + aimMachines.Key.Status + "]");
+
+                        foreach (var aimMachine in aimMachines)
+                        {
+                            output.AppendLine("(" + aimMachine.CoinPrice + "円) " + aimMachine.StoreName + " " + aimMachine.StartCount + "G");
+                            output.AppendLine(aimMachine.SlotMachineUri.ToString());
+                            outputUri.Add(aimMachine.SlotMachineUri.ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    continue;
+                    output.AppendLine("狙える台は無いンゴなぁ");
+                }
+            }
+
+            output.AppendLine();
+            return (output.ToString(), outputUri.OrderBy(x => x).ToList());
+        }
+        #endregion
+    }
+}
---
+++ SlotAddictionConsoleLogic/Models/AnalyseSlotData.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SlotAddictionCore.Extentions;
+using SlotAddictionCore.Models;
+using SlotAddictionCore.Models.Scrapings;
+
+namespace SlotAddictionLogic.Models
+{
+    public class AnalyseSlotData
+    {
+        #region フィールド
+        /// <summary>
+        /// usingステートメントを使用して何度もソケットを開放するとリソースを食いつぶす為staticなHttpClientを作成する
+        /// </summary>
+        private static readonly HttpClient _httpClient = new HttpClient();
+        /// <summary>
+        /// HTML解析オブジェクト
+        /// </summary>
+        private readonly Goraggio _analysisHTML = new Goraggio();
+        /// <summary>
+        /// データ取得日
+        /// </summary>
+        private readonly DateTime _dataDate;
+        /// <summary>
+        /// 検索対象の店舗情報
+        /// </summary>
+        private List<TenpoInfo> _tenpoInfoList;
+        /// <summary>
+        /// 検索する機種名
+        /// </summary>
+        private List<string> _slotMachineSearchNames;
+        /// <summary>
+        /// 狙い目台の情報
+        /// </summary>
+        private List<AimMachineInfo> _aimMachineList;
+        /// <summary>
+        /// フロアを検索するURI
+        /// </summary>
+        private readonly HashSet<Uri> _floorSearchUri = new HashSet<Uri>();
+        /// <summary>
+        /// 遊技台のURI
+        /// </summary>
+        private readonly HashSet<Uri> _slotDataUri = new HashSet<Uri>();
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// 遊技履歴を格納
+        /// </summary>
+        public HashSet<SlotPlayData> SlotPlayDataCollection { get; set; }
+        /// <summary>
+        /// 狙い目の台を格納
+        /// </summary>
+        public HashSet<SlotPlayData> AimMachineCollection { get; set; }
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataDate"></param>
+        public AnalyseSlotData(DateTime dataDate)
+        {
+            ServicePointManager.DefaultConnectionLimit = 1000;
+            _dataDate = dataDate;
+        }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// 遊技台のデータ取得
+        /// </summary>
+        /// <param name="tenpoInfoList"></param>
+        /// <param name="slotMachineSearchNames"></param>
+        /// <param name="aimMachineInfo"></param>
+        /// <returns></returns>
+        public async Task GetSlotDataAsync(List<TenpoInfo> tenpoInfoList, List<string> slotMachineSearchNames, List<AimMachineInfo> aimMachineInfo)
+        {
+            SlotPlayDataCollection = new HashSet<SlotPlayData>();
+            AimMachineCollection = new HashSet<SlotPlayData>();
+
+            _tenpoInfoList = tenpoInfoList;
+            _slotMachineSearchNames = slotMachineSearchNames;
+            _aimMachineList = aimMachineInfo;
+
+            //foreach (var tempo in _tenpoInfoList.Where(x => x.TenpoUri.ToString() == "https://daidata.goraggio.com/100247/"))
+            foreach (var tempo in _tenpoInfoList)
+            {
+                //初期化
+                _floorSearchUri.Clear();
+                _slotDataUri.Clear();
+
+                //一旦置いとく
+                //var slotMachineStartNo = tempo.SlotMachineStartNo;
+                //var slotMachineEndNo = tempo.SlotMachineEndNo;
+                //var slotMachineNumbers = Enumerable.Range(slotMachineStartNo, slotMachineEndNo - slotMachineStartNo + 1);
+                var slotMachineNumbers = Enumerable.Empty<int>();
+
+                if (_slotMachineSearchNames != null)
+                {
+                    //指定した台が店舗に存在するか確かめられるURIを作成
+                    var floorSearchUri = _slotMachineSearchNames.Select(slotMachineSearchName => new Uri($"{tempo.TenpoUri.ToString()}unit_list?model={slotMachineSearchName}"));
+                    _floorSearchUri.AddRange(await floorSearchUri.GetExistUriAsync(_httpClient));
+
+                    //指定した台が無ければ次の店舗の検索をする
+                    if (!_floorSearchUri.Any()) continue;
+
+                    try
+                    {
+                        var floorStreamTasks = _floorSearchUri.Select(floorUri => _httpClient.GetStreamAsync(floorUri));
+                        var streamResponses = await Task.WhenAll(floorStreamTasks);
+
+                        //取得したソースを解析
+                        var floorAnalyseTasks = streamResponses.Select(response => _analysisHTML.AnalyseFloorAsync(response));
+                        var slotMachineNumbersForSlotModels = await Task.WhenAll(floorAnalyseTasks);
+
+                        //リストの平準化
+                        slotMachineNumbers = slotMachineNumbersForSlotModels.SelectMany(x => x);
+                        //slotMachineNumbers = Enumerable.Repeat(532, 1);
+                    }
+                    catch
+                    {
+                        //指定した台が無ければ次の店舗の検索をする
+                        continue;
+                    }
+                }
+
+                try
+                {
+                    //遊技台の情報があるURIを作成する
+                    var month = $"{_dataDate.Month:D2}";
+                    var day = $"{_dataDate.Day:D2}";
+                    var slotDataUri = slotMachineNumbers
+                        .Select(slotMachineNumber => new Uri(
+                            $"{tempo.TenpoUri.ToString()}detail?unit={slotMachineNumber}&target_date={_dataDate.Year}-{month}-{day}"));
+                    _slotDataUri.AddRange(await slotDataUri.GetExistUriAsync(_httpClient));
+
+                    //存在するURI内のソースを取得
+                    var slotDataStreamTasks = _slotDataUri
+                        .Select(uri => _httpClient.GetStreamAsync(uri));
+                    var streamResponses = await Task.WhenAll(slotDataStreamTasks);
+
+                    //取得したソースを解析
+                    var slotDataAnalyseTasks =
+                        streamResponses.Select((response, index)
+                            => _analysisHTML.AnalyseAsync(response, tempo, _slotMachineSearchNames, _aimMachineList, _slotDataUri.ElementAt(index)));
+                    var analysisSlotData = await Task.WhenAll(slotDataAnalyseTasks);
+
+                    //解析したデータをコレクションに追加
+                    SlotPlayDataCollection.AddRange(analysisSlotData.Where(x => x != null));
+
+                    //狙い目の台をコレクションに追加
+                    AimMachineCollection.AddRange(SlotPlayDataCollection.Where(x => x.Status != null));
+                    foreach (var slotData in SlotPlayDataCollection)
+                    {
+
+                        //if (slotData.Title.Contains("凱旋"))
+                        //{
+                        //    //前日G数 + 初当たりG数(または現在のスタート回数)が1504G+10G(ペナ考慮)を超えていればリセットされている
+                        //    var isReset = slotData.FinalStartCountYesterDay + firstStartCount > 1504 + 10;
+
+                        //    //リセットされているかつ初当たりまで1024Gを超えていたら高設定確定
+                        //    if (isReset)
+                        //    {
+                        //        //当選履歴がない場合はこの判断ができないはずなので除外
+                        //        //10Gはペナ考慮
+                        //        if (firstStartCount > 1024 + 10 && slotData.IsPublicWinningHistory)
+                        //        {
+                        //            //高設定であれば追加する
+                        //            slotData.Status = "456確定";
+                        //            AimMachineCollection.Add(slotData);
+                        //        }
+                        //        else if (slotData.ARTCount + slotData.BigBonusCount + slotData.RegulerBonusCount == 0)
+                        //        {
+                        //            //リセット確定の場合は当日未当選の台のみを追加する
+                        //            AimMachineCollection.Add(slotData);
+                        //        }
+                        //    }
+                        //}
+                        //else
+                        //{
+                            //狙い目の台をコレクションに追加
+                           
+                        //}
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    var check = _floorSearchUri;
+                    var check2 = _slotDataUri;
+
+                    //他に何があるんやろうか
+                    throw;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
